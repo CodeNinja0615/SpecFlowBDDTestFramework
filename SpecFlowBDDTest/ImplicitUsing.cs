e7447f73db1d8607c61d761cd6ba84f215a97d01
Sameer <akhtarsameer743@gmail.com>
2025-03-30 23:32:00 +0530

    Added global namespaces file and git ingnore changes

---
+++ SpecFlowBDDTest/ImplicitUsing.cs
@@ -0,0 +1,6 @@
+//--Global namespace file
+global using OpenQA.Selenium;
+global using NUnit.Framework;
+global using TechTalk.SpecFlow;
+global using SeleniumExtras.PageObjects;
+global using OpenQA.Selenium.Support.UI;
